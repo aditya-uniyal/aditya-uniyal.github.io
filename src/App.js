01897ec2adcc85aaf8bff82d18b63a153d3e10f6
aditya-uniyal <uniyal.aditya61@gmail.com>
2023-07-01 18:21:48 -0300

    fixed bugs.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Contact from './components/Contact'
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
