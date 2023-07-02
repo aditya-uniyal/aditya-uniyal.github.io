0c06c5eed7fa9333f358941d3f01099f648b8467
aditya-uniyal <uniyal.aditya61@gmail.com>
2023-07-01 21:49:06 -0300

    added responsiveness

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,50 +1,107 @@
-import './index.scss';
-import { Link, NavLink } from 'react-router-dom';
+import './index.scss'
+import { Link, NavLink } from 'react-router-dom'
 // import LogoS from '../../assets/images/logo-a.png';
-import LogoA from '../../assets/images/logo-sidebar.png';
+import LogoA from '../../assets/images/logo-sidebar.png'
+import { useState } from 'react'
+import LogoSubtitle from '../../assets/images/logo-sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faBars,
+  faClose,
+  faEnvelope,
+  faHome,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons'
+import {
+  faGithub,
+  faInstagram,
+  faLinkedin,
+} from '@fortawesome/free-brands-svg-icons'
 
-import LogoSubtitle from '../../assets/images/logo-sub.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
-import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false)
 
-const Sidebar = () => (
-    <div className='nav-bar'>
-        <Link className='logo' to='/'>
-            <img src={LogoA} alt="logo" />
-            <img src={LogoSubtitle} alt="Aditya" className='sub-logo' />
-        </Link>
-        
-        <nav>
-            <NavLink exact="true" activeClassName="active" to="/">
-                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-            </NavLink>
-            <NavLink className="about-link" exact="true" activeClassName="active" to="/about">
-                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-            </NavLink>
-            <NavLink className="contact-link" exact="true" activeClassName="active" to="/contact">
-                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-            </NavLink>
-        </nav>
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoA} alt="logo" />
+        <img src={LogoSubtitle} alt="Aditya" className="sub-logo" />
+      </Link>
 
-        <ul>
-            <li>
-                <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/aditya-uniyal-bbb85015a/">
-                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-                </a>
-            </li>
-            <li>
-                <a target="_blank" rel="noreferrer" href="https://github.com/aditya-uniyal">
-                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
-                </a>
-            </li>
-            <li>
-                <a target="_blank" rel="noreferrer" href="https://www.instagram.com/aditya7449/">
-                    <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
-                </a>
-            </li>
-        </ul>
+      <nav className={showNav ? 'mobile-show' : ''}>
+        <NavLink
+          exact="true"
+          activeClassName="active"
+          to="/"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          onClick={() => setShowNav(false)}
+          className="about-link"
+          exact="true"
+          activeClassName="active"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          onClick={() => setShowNav(false)}
+          className="contact-link"
+          exact="true"
+          activeClassName="active"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+        <FontAwesomeIcon
+          onClick={() => setShowNav(false)}
+          icon={faClose}
+          color="#ffd700"
+          size="3x"
+          className="close-icon"
+        />
+      </nav>
+
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/aditya-uniyal-bbb85015a/"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/aditya-uniyal"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.instagram.com/aditya7449/"
+          >
+            <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+      <FontAwesomeIcon
+        onClick={() => setShowNav(true)}
+        icon={faBars}
+        color="#ffd700"
+        size="3x"
+        className="hamburger-icon"
+      />
     </div>
-);
+  )
+}
 
-export default Sidebar;
+export default Sidebar
